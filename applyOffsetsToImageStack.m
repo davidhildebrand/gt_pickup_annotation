a54632da2f7c6608042c6adb80673a9e9988379b
Jasper Maniates-Selvin <jtmaniatesselvin@g.harvard.edu>
2019-08-08 16:13:15 -0400

    applyOffsetsToImageStack now has parameters more appropriately named and pulled out into a common location for easier editing.

--- applyOffsetsToImageStack.m
+++ applyOffsetsToImageStack.m
@@ -1,12 +1,22 @@
-cd /n/groups/htem/temcagt/datasets/vnc1_r066/roi_generation/2_crop
+%First, mount the dataset directory locally using sshfs, or download the
+%files that need to be aligned.
 
-for i = vnc1rawToSlotslotToSectionrawToSectionoffsets(929:end,:)'
-    imname=ls(['*_' num2str(i(1)) '_section.tif']);
+%cd to the location of the raw (unaligned) stainer images
+cd /n/groups/htem/temcagt/datasets/aedes_r195/roi_generation/stainer_images_1_correctedBarcodes
+
+%Load the required translations (output by the Align images in stack...
+%Fiji plugin).
+offsets=readmatrix('../alignment_try2');
+dXcolumn=2;
+dYcolumn=3;
+sectionNumColumn=4;
+
+for i = offsets'
+    imname=ls(['*_' num2str(i(sectionNumColumn)) '_section.png']);
     imname=imname(1:length(imname)-1)
     imdata=imread(imname);
-    %imdata=imread([num2str(i(1)) '.png']);
-    alignedimage=imtranslate(imdata, [i(2) i(3)]);
+    alignedimage=imtranslate(imdata, [i(dXcolumn) i(dYcolumn)]);
     imshow(alignedimage)
-    pause(.1)
-    imwrite(alignedimage,['/n/groups/htem/temcagt/datasets/vnc1_r066/roi_generation/verifying_rawToSectionOffsets/' imname]);
+    pause(.05)
+    imwrite(alignedimage,['/n/groups/htem/temcagt/datasets/aedes_r195/roi_generation/stainer_images_4_slot_aligned_color/' imname]);
 end
