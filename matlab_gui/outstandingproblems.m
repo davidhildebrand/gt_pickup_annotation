fed1d7c824dc85ff4b28a1cc267c21a51bd150c6
David Hildebrand <david@hildebrand.name>
2022-05-05 14:31:24 -0400

    initial cleanup

--- matlab_gui/outstandingproblems.m
+++
@@ -1,6 +0,0 @@
-% problem: txt file name does not match image name exactly: leading 0s
-
-% open file
-f = fullfile(outputPath,[num2str(secID),'.txt']);
-
-%% can't save into section 0 (indexing fail)
